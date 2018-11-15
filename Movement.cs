f7c985ff2376abcf829b895e9cf0d92747ec67c5
JoshMD24 <44683526+JoshMD24@users.noreply.github.com>
2018-11-15 16:43:01 -0500

    Update Movement.cs

--- Movement.cs
+++ Movement.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+//This is a basic movement script that i use for my car
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -17,7 +19,7 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Move on the x axis                              0 is the y axis          //Move on the y axis
+        //Move on the x axis                                                     0 is the y axis          Move on the y axis
         transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
 
 
